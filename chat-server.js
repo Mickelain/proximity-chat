ffac31faea6bbbf8cd543419da7ec65dc3af1fc6
Mickelain <mclain.calvin@gmail.com>
2016-09-10 02:39:41 -0400

    Added comments to chat-server

--- chat-server.js
+++ chat-server.js
@@ -10,9 +10,13 @@ var chatServer = function(server) {
     var io = require('socket.io').listen(server);
     io.on('connection', function(socket){
         console.log('a user connected');
+
+        // On disconnect
         socket.on('disconnect', function(){
             console.log('a user disconnected');
         });
+
+        // On message send
         socket.on('chat message', function(msg) {
             io.emit('chat message', msg);
         })
