68566b1cc7e8af2ba301ba5a98b5b95957b3ca06
Mickelain <mclain.calvin@gmail.com>
2016-09-09 22:22:45 -0400

    Reverting changes of the intentional test fail

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var chat = require('./routes/chat';
+var chat = require('./routes/chat');
 
 var app = express();
 
